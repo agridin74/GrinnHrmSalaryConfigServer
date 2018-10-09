c337576832d18ad44a4cd08dcf2a7346b5e0dcd5
Alex Gridin <agridin74@gmail.com>
2018-10-09 09:59:02 +0300

    Initial commit

---
+++ GrinnHrmSalaryConfigServer/src/main/java/ru/grinncorp/configserver/GrinnHrmSalaryConfigServerApplication.java
@@ -0,0 +1,12 @@
+package ru.grinncorp.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrinnHrmSalaryConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrinnHrmSalaryConfigServerApplication.class, args);
+	}
+}
