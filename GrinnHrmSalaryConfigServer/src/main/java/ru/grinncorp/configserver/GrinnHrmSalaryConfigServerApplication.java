cde47477353656a83967653b44103d3968be87a3
Alex Gridin <agridin74@gmail.com>
2018-10-19 13:35:42 +0300

    Add log

--- GrinnHrmSalaryConfigServer/src/main/java/ru/grinncorp/configserver/GrinnHrmSalaryConfigServerApplication.java
+++ GrinnHrmSalaryConfigServer/src/main/java/ru/grinncorp/configserver/GrinnHrmSalaryConfigServerApplication.java
@@ -4,6 +4,8 @@ package ru.grinncorp.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Класс сервера конфигурации
@@ -15,9 +17,12 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class GrinnHrmSalaryConfigServerApplication {
+	
+	private static Logger logger = LoggerFactory.getLogger(GrinnHrmSalaryConfigServerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrinnHrmSalaryConfigServerApplication.class, args);
+		logger.info("Start Config Server....");
 		
 	}
 }
--- GrinnHrmSalaryDiscoveryServer/src/main/java/ru/grinncorp/eurekaserver/GrinnHrmSalaryDiscoveryServerApplication.java
+++ GrinnHrmSalaryDiscoveryServer/src/main/java/ru/grinncorp/eurekaserver/GrinnHrmSalaryDiscoveryServerApplication.java
@@ -3,6 +3,8 @@ package ru.grinncorp.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author agridin74
@@ -11,8 +13,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class GrinnHrmSalaryDiscoveryServerApplication {
+	
+	private static Logger logger = LoggerFactory.getLogger(GrinnHrmSalaryDiscoveryServerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrinnHrmSalaryDiscoveryServerApplication.class, args);
+		logger.info("Start Eureka Server ... ");
 	}
 }
